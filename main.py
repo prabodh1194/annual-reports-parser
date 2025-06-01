f96ff31078b8933f052f80abc336fe90dae6cdb2
Prabodh Agarwal <agarwal_p@apple.com>
2025-06-01 12:55:36 +0530

    some more changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from embeddings.from_parquet import from_parquet, store_embedding
 from extract_text import extract_text_from_all_pages
-from query import query
 
 pdf_path = "/Users/pbd/ar/2024/dixon.pdf"
 
@@ -9,5 +8,3 @@ extract_text_from_all_pages(pdf_path)
 # generate_embeddings( pdf_path=pdf_path, report_year="2024", company_name="Dixon Technologies" )
 
 db = store_embedding(*from_parquet("~/ar/2024/dixon_embeddings.parquet"))
-
-query(db)
