bd74631e0e9863ab535cb2c766c756a19de632da
Prabodh Agarwal <agarwal_p@apple.com>
2025-06-01 14:37:49 +0530

    refactor the embedding function

--- embeddings/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import chromadb.utils.embedding_functions as embedding_functions
-
-openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-    api_key="abc123",
-    api_base=f"http://{os.getenv('model_url')}:8000/v1",
-    model_name="/home/ubuntu/model",
-)
