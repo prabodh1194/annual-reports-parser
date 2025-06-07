c12af162956bc906a58902eea85674916d99caba
Prabodh Agarwal <agarwal_p@apple.com>
2025-06-07 12:13:39 +0530

    use a URL

--- embeddings/chroma_ef.py
+++ embeddings/chroma_ef.py
@@ -1,11 +1,7 @@
-import os
-
 import chromadb.utils.embedding_functions as embedding_functions
-
-assert os.getenv("model_url"), "missing url for where the model has been hosted"
 
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key="abc123",
-    api_base=f"http://{os.getenv('model_url')}:8000/v1",
+    api_base="http://emb.openn.ai:8000/v1",
     model_name="/home/ubuntu/model",
 )
